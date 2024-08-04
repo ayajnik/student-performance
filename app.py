4bb1709e9e755e0197cbe1413f9112e4c83107c0
ayajnik <ayush.yajnik@yahoo.com>
2024-08-04 14:45:53 -0400

    adding notebook

---
+++ app.py
@@ -0,0 +1,11 @@
+try:
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    import pandas as pd
+    import numpy as np
+    import sys
+    print("\n")
+    print("Libraries imported")
+    print("\n")
+except ImportError as e:
+    print(e,sys)
